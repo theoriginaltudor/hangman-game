771c0dde8d3086f5b30092a45f77d8a8066b7493
theoriginaltudor <theoriginaltudor@gmail.com>
2025-01-24 09:37:01 +0100

    feat: replace button with Link in Category component for navigation

--- app/components/selectable-category.tsx
+++ app/components/selectable-category.tsx
@@ -1,12 +1,18 @@
+import { Link } from "react-router";
+import { Routes } from "~/lib/utils";
+
 export const Category = ({ category }: { category: string }) => {
   return (
-    <button className="flex justify-center bg-darkNavy relative py-[4.8125rem] w-full rounded-3xl hover:brightness-125">
+    <Link
+      to={`${Routes.ingame}/${category}`}
+      className="flex justify-center bg-darkNavy relative py-[1.875rem] md:py-[4.625rem] xl:py-[4.8125rem] w-full rounded-3xl hover:brightness-125"
+    >
       <div className="flex justify-center absolute rounded-3xl bg-blue-500 top-[0.0625rem] bottom-[0.3125rem] left-[0.1875rem] right-[0.1875rem]">
         <div className="flex justify-center absolute top-[0.375rem] bottom-0 rounded-3xl bg-playBlue left-[0.1875rem] right-[0.1875rem]" />
       </div>
-      <h2 className="uppercase font-mouse text-mm leading-none text-white relative">
+      <h2 className="uppercase font-mouse text-[1.5rem] md:text-mm leading-none text-white relative tracking-wider">
         {category}
       </h2>
-    </button>
+    </Link>
   );
 };
