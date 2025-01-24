acec7a89e550a509c2037cb55012c8bb82f3d65c
theoriginaltudor <theoriginaltudor@gmail.com>
2025-01-24 12:20:57 +0100

    feat: add keyboard letter component for user interaction and update game layout

--- app/components/keyboard-letter.tsx
+++ app/components/keyboard-letter.tsx
@@ -1,17 +1,22 @@
 export const KeyboardLetter = ({
   letter,
   disabled,
+  action,
 }: {
   letter: string;
   disabled?: boolean;
+  action: () => void;
 }) => {
   return (
     <button
+      onMouseDown={action}
       className={`${
         disabled ? "brightness-50 " : "hover:bg-playBlue hover:text-white "
-      }flex justify-center  text-darkNavy  bg-white rounded-[1.875rem] py-[1.5625rem] px-8`}
+      }flex justify-center  text-darkNavy  bg-white rounded-lg md:rounded-[1.875rem] py-[1.1875rem] md:py-[1.5625rem] px-2.5 md:px-8`}
     >
-      <h2 className="uppercase font-mouse text-mm leading-none">{letter}</h2>
+      <h2 className="uppercase font-mouse text-2xl md:text-mm leading-none">
+        {letter}
+      </h2>
     </button>
   );
 };
--- app/routes/game.tsx
+++ app/routes/game.tsx
@@ -1,6 +1,8 @@
 import { useLoaderData, type LoaderFunctionArgs } from "react-router";
 import data from "app/DB/data.json";
 import { PlayableLetter } from "~/components/playable-letter";
+import { KeyboardLetter } from "~/components/keyboard-letter";
+import { useState } from "react";
 
 type CategoryKeys = keyof typeof data.categories;
 
@@ -13,17 +15,39 @@ export async function loader({ params }: LoaderFunctionArgs) {
 
 const Game = () => {
   const { guess } = useLoaderData<typeof loader>();
+  const [selected, setSelected] = useState<string[]>([]);
   return (
-    <div className="flex flex-wrap gap-x-3 gap-y-2 md:gap-x-4 md:gap-y-3 xl:gap-y-3 justify-center">
-      {guess.name.split("").map((letter, index) => {
-        if (letter === " ")
-          return (
-            <div className="opacity-0">
-              <PlayableLetter key={index} letter={letter} />
-            </div>
-          );
-        return <PlayableLetter key={index} letter={letter} />;
-      })}
+    <div className="flex flex-col gap-28">
+      <div className="flex flex-wrap gap-x-3 gap-y-2 md:gap-x-4 md:gap-y-3 xl:gap-y-3 justify-center">
+        {guess.name
+          .toLowerCase()
+          .split("")
+          .map((letter, index) => {
+            if (letter === " ")
+              return (
+                <div className="opacity-0">
+                  <PlayableLetter key={index} letter={letter} />
+                </div>
+              );
+            return (
+              <PlayableLetter
+                key={index}
+                letter={letter}
+                disabled={!selected.includes(letter)}
+              />
+            );
+          })}
+      </div>
+      <div className="flex flex-wrap gap-x-2 gap-y-6">
+        {"abcdefghijklmnopqrstuvwxyz".split("").map((letter) => (
+          <KeyboardLetter
+            key={letter}
+            letter={letter}
+            action={() => setSelected([...selected, letter])}
+            disabled={selected.includes(letter)}
+          />
+        ))}
+      </div>
     </div>
   );
 };
