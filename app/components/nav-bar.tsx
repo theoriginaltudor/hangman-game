480e08c91c3441c0fea95680da8969ae45033aa1
theoriginaltudor <theoriginaltudor@gmail.com>
2025-01-24 09:11:41 +0100

    feat: implement NavBar component for improved navigation layout

---
+++ app/components/nav-bar.tsx
@@ -0,0 +1,16 @@
+import { BackButton } from "./back-button";
+import { ShadowLetter } from "./shadow-letter";
+
+export const NavBar = ({ pageHeading }: { pageHeading: string }) => {
+  return (
+    <div className="flex justify-between items-center">
+      <BackButton />
+      <div>
+        {pageHeading !== "" &&
+          Array.from(pageHeading).map((letter, index) => (
+            <ShadowLetter letter={letter} key={index} />
+          ))}
+      </div>
+    </div>
+  );
+};
--- app/layouts/nav-layout.tsx
+++ app/layouts/nav-layout.tsx
@@ -1,6 +1,5 @@
 import { Outlet, useLoaderData, type LoaderFunctionArgs } from "react-router";
-import { BackButton } from "~/components/back-button";
-import { ShadowLetter } from "~/components/shadow-letter";
+import { NavBar } from "~/components/nav-bar";
 import { Routes, type RouteValues } from "~/lib/utils";
 
 export async function loader({ request }: LoaderFunctionArgs) {
@@ -22,15 +21,7 @@ const NavLayout = () => {
   const { pageHeading } = useLoaderData<typeof loader>();
   return (
     <div className="h-screen flex flex-col gap-20 px-6 py-8 md:px-10 md:py-16 xl:px-28 xl:py-20">
-      <div className="flex justify-between items-center">
-        <BackButton />
-        <div>
-          {pageHeading !== "" &&
-            Array.from(pageHeading).map((letter, index) => (
-              <ShadowLetter letter={letter} key={index} />
-            ))}
-        </div>
-      </div>
+      <NavBar pageHeading={pageHeading} />
       <Outlet />
     </div>
   );
