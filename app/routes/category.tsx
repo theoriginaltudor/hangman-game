371abf9515b2301089b4849c24a20ecee81a3a41
theoriginaltudor <theoriginaltudor@gmail.com>
2025-01-24 09:26:33 +0100

    feat: add ChooseCategory component with category loader and data integration

--- app/routes/category.tsx
+++ app/routes/category.tsx
@@ -1,3 +1,20 @@
-export const ChooseCategory = () => {
-  return <div>ChooseCategory</div>;
+import data from "app/DB/data.json";
+import { useLoaderData } from "react-router";
+import { Category } from "~/components/selectable-category";
+
+export async function loader() {
+  const categories = Object.keys(data.categories);
+  return { categories };
+}
+
+const ChooseCategory = () => {
+  const { categories } = useLoaderData<typeof loader>();
+  return (
+    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-8 xl:grid-cols-3 xl:gap-x-12 xl: gap-y-8">
+      {categories.map((category) => (
+        <Category key={category} category={category} />
+      ))}
+    </div>
+  );
 };
+export default ChooseCategory;
