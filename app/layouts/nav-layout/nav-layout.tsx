9e10b038ca6b82ff030fd480ba1eda0f7b16b35b
theoriginaltudor <theoriginaltudor@gmail.com>
2025-01-27 16:21:57 +0100

    feat: add keyboard navigation to navigate back with Escape and Backspace keys in NavLayout

--- app/layouts/nav-layout/nav-layout.tsx
+++ app/layouts/nav-layout/nav-layout.tsx
@@ -1,6 +1,12 @@
-import { Outlet, useLoaderData, type LoaderFunctionArgs } from "react-router";
+import {
+  Outlet,
+  useLoaderData,
+  useNavigate,
+  type LoaderFunctionArgs,
+} from "react-router";
 import { Routes, type RouteValues } from "~/lib/utils";
 import { NavBar } from "./nav-bar";
+import { useEffect } from "react";
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const url = new URL(request.url);
@@ -19,6 +25,19 @@ export async function loader({ request }: LoaderFunctionArgs) {
 
 const NavLayout = () => {
   const { pageHeading } = useLoaderData<typeof loader>();
+  const navigate = useNavigate();
+  const keyPressHandler = (e: KeyboardEvent) => {
+    if (e.key === "Escape" || e.key === "Backspace") {
+      navigate(-1);
+    }
+  };
+  useEffect(() => {
+    document.addEventListener("keydown", keyPressHandler);
+
+    return () => {
+      document.removeEventListener("keydown", keyPressHandler);
+    };
+  }, []);
   return (
     <div className="h-screen flex flex-col gap-20 px-6 py-8 md:px-10 md:py-16 xl:px-28 xl:py-20">
       <NavBar pageHeading={pageHeading} />
