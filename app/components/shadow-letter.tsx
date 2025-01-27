c9b6f86947b4f9c34fefc6c995384ec8e3117d98
theoriginaltudor <theoriginaltudor@gmail.com>
2025-01-27 12:11:27 +0100

    feat: enhance ShadowLetter component with title prop and utility class merging; add ChooseCategory route for category selection

--- app/components/shadow-letter.tsx
+++ app/components/shadow-letter.tsx
@@ -1,19 +1,30 @@
-export const ShadowLetter = ({ letter }: { letter: string }) => {
+import { cn } from "~/lib/utils";
+
+export const ShadowLetter = ({
+  letter,
+  title,
+}: {
+  letter: string;
+  title?: boolean;
+}) => {
   if (letter === " ") {
     return <span className="opacity-0">0</span>;
   }
-  const shadow = {
-    sm: "text-stroke-sm text-mm",
-    md: "text-stroke-lg text-[6.5rem]",
-    xl: "text-stroke-lg text-mxl",
-  };
 
   return (
     <div className="relative inline-flex p-0.5 xl:p-1">
-      <span className="font-mouse tracking-wider text-stroke-sm md:text-stroke-lg text-mm md:text-[6.5rem] xl:text-mxl">
+      <span
+        className={cn(
+          "font-mouse tracking-wider text-stroke-sm md:text-stroke-lg text-mm md:text-[6.5rem] xl:text-mxl"
+        )}
+      >
         {letter}
       </span>
-      <span className="bg-gradient-to-t from-[#E3F2FF] to-playSky font-mouse absolute tracking-widest text-transparent bg-clip-text text-mm md:text-[6.5rem] xl:text-mxl">
+      <span
+        className={cn(
+          "bg-gradient-to-t from-[#E3F2FF] to-playSky font-mouse absolute tracking-widest text-transparent bg-clip-text text-mm md:text-[6.5rem] xl:text-mxl"
+        )}
+      >
         {letter}
       </span>
     </div>
