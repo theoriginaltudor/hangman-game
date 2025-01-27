04583619c93c483e3620c546e3d5c7105dec8a38
theoriginaltudor <theoriginaltudor@gmail.com>
2025-01-27 13:01:09 +0100

    feat: update MenuDialog to accept title prop for dynamic message display

--- app/routes/game/layout/menu-dialog.tsx
+++ app/routes/game/layout/menu-dialog.tsx
@@ -4,13 +4,13 @@ import { PurpleButton } from "../components/purple-button";
 import { Routes } from "~/lib/utils";
 import { ShadowLetter } from "~/components/shadow-letter";
 
-export const MenuDialog = () => {
+export const MenuDialog = ({ title }: { title: string }) => {
   return (
     <div className="h-screen flex fixed left-0 items-center justify-center w-screen">
       <div className="absolute bg-gradient-to-t from-[#2B1677] to-[#1A043A] h-screen opacity-75 w-screen" />
       <Card className="gap-y-14 pb-[3.25rem]">
         <div className="relative w-[16.4375rem] md:w-[23.3125rem] -mt-[5rem] md:-mt-[6.5rem]">
-          {"You Lose".split("").map((letter, index) => (
+          {title.split("").map((letter, index) => (
             <ShadowLetter key={index} letter={letter} title />
           ))}
         </div>
