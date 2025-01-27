fa05e5c44715d90f3355103eaecf9a2c7445149f
theoriginaltudor <theoriginaltudor@gmail.com>
2025-01-27 13:51:01 +0100

    feat: enhance BlueButton component to support action prop and update MenuDialog to utilize game state for button actions

--- app/components/blue-button.tsx
+++ app/components/blue-button.tsx
@@ -3,8 +3,24 @@ import { Link } from "react-router";
 
 export const BlueButton: React.FC<{
   children: React.ReactNode;
-  to: string;
-}> = ({ children, to: link }) => {
+  to?: string;
+  action?: () => void;
+}> = ({ children, to: link, action }) => {
+  if (!link) {
+    return (
+      <button
+        {...(action && { onMouseDown: action })}
+        className="flex justify-center bg-darkNavy relative py-5 px-16 rounded-full hover:brightness-125"
+      >
+        <div className="flex justify-center absolute rounded-full bg-blue-500 top-[0.0625rem] bottom-[0.3125rem] left-[0.1875rem] right-[0.1875rem]">
+          <div className="flex justify-center absolute top-[0.375rem] bottom-0 rounded-full bg-playBlue left-[0.1875rem] right-[0.1875rem]" />
+        </div>
+        <h2 className="uppercase font-mouse text-ms leading-none text-white relative tracking-widest">
+          {children}
+        </h2>
+      </button>
+    );
+  }
   return (
     <Link
       to={link}
--- app/routes/game/layout/menu-dialog.tsx
+++ app/routes/game/layout/menu-dialog.tsx
@@ -3,8 +3,10 @@ import { Card } from "~/components/transparent-card";
 import { PurpleButton } from "../components/purple-button";
 import { Routes } from "~/lib/utils";
 import { ShadowLetter } from "~/components/shadow-letter";
+import { useGameStateStore } from "../stores/game-state-store";
 
 export const MenuDialog = ({ title }: { title: string }) => {
+  const { state, updateState } = useGameStateStore();
   return (
     <div className="h-screen flex fixed left-0 items-center justify-center w-screen">
       <div className="absolute bg-gradient-to-t from-[#2B1677] to-[#1A043A] h-screen opacity-75 w-screen" />
@@ -14,7 +16,13 @@ export const MenuDialog = ({ title }: { title: string }) => {
             <ShadowLetter key={index} letter={letter} title />
           ))}
         </div>
-        <BlueButton to="/instructions">Continue</BlueButton>
+        <BlueButton
+          {...(state === "paused" && { action: () => updateState("playing") })}
+          {...(state === "won" && { to: Routes.home })}
+          {...(state === "lost" && { to: Routes.home })}
+        >
+          Continue
+        </BlueButton>
         <BlueButton to={Routes.category}>New category</BlueButton>
         <PurpleButton to={Routes.home}>Quit game</PurpleButton>
       </Card>
