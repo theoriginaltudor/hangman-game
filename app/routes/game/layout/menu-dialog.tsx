949075975567e4ec10671c33dcf448ec35993e10
theoriginaltudor <theoriginaltudor@gmail.com>
2025-01-27 12:57:22 +0100

    fix: correct typo in MenuDialog message and update ShadowLetter tracking class

--- app/routes/game/layout/menu-dialog.tsx
+++ app/routes/game/layout/menu-dialog.tsx
@@ -10,7 +10,7 @@ export const MenuDialog = () => {
       <div className="absolute bg-gradient-to-t from-[#2B1677] to-[#1A043A] h-screen opacity-75 w-screen" />
       <Card className="gap-y-14 pb-[3.25rem]">
         <div className="relative w-[16.4375rem] md:w-[23.3125rem] -mt-[5rem] md:-mt-[6.5rem]">
-          {"You Louse".split("").map((letter, index) => (
+          {"You Lose".split("").map((letter, index) => (
             <ShadowLetter key={index} letter={letter} title />
           ))}
         </div>
