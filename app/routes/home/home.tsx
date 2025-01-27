e257b524dbf1d01d10f85c89a01981046ff59a52
theoriginaltudor <theoriginaltudor@gmail.com>
2025-01-27 16:14:58 +0100

    feat: add keyboard navigation for Enter and 'h' keys in Home component

--- app/routes/home/home.tsx
+++ app/routes/home/home.tsx
@@ -4,6 +4,8 @@ import { PlayButton } from "~/routes/home/play-button";
 import { BlueButton } from "~/components/blue-button";
 import type { Route } from "../../+types/root";
 import { Routes } from "~/lib/utils";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -13,6 +15,26 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const navigate = useNavigate();
+  const enterPressHandler = (e: KeyboardEvent) => {
+    if (e.key === "Enter") {
+      navigate(Routes.category);
+    }
+  };
+  const hPressHandler = (e: KeyboardEvent) => {
+    if (e.key === "h") {
+      navigate(Routes.instructions);
+    }
+  };
+  useEffect(() => {
+    document.addEventListener("keydown", enterPressHandler);
+    document.addEventListener("keydown", hPressHandler);
+
+    return () => {
+      document.removeEventListener("keydown", enterPressHandler);
+      document.removeEventListener("keydown", hPressHandler);
+    };
+  }, []);
   return (
     <div className="flex flex-col pt-40 items-center h-screen">
       <Card className="gap-y-14 pb-[3.25rem]">
