5dc4b05ce92be722a3173e5f5e4480994ebb27d1
theoriginaltudor <theoriginaltudor@gmail.com>
2025-01-29 12:28:09 +0100

    feat: update button components in KeyboardLetter and PlayableLetter for improved styling and responsiveness

--- app/routes/game/components/keyboard-letter.tsx
+++ app/routes/game/components/keyboard-letter.tsx
@@ -13,7 +13,7 @@ export const KeyboardLetter = ({
     <button
       onMouseDown={action}
       className={cn(
-        "flex justify-center text-darkNavy bg-white rounded-lg md:rounded-[1.875rem] py-[1.1875rem] md:py-[1.5625rem] w-[1.8125rem] md:w-16 xl:w-[6.8125rem]",
+        "flex justify-center text-darkNavy bg-white rounded-lg md:rounded-3xl xl:rounded-[1.875rem] py-[1.1875rem] md:py-[1.5625rem] w-[1.8125rem] md:w-16 xl:w-[6.8125rem]",
         {
           "brightness-50": disabled,
           "hover:bg-playBlue hover:text-white": !disabled,
--- app/routes/game/components/playable-letter.tsx
+++ app/routes/game/components/playable-letter.tsx
@@ -10,14 +10,14 @@ export const PlayableLetter = ({
   return (
     <button
       className={cn(
-        "flex justify-center bg-darkNavy relative rounded-[0.75rem] md:rounded-[2.5rem] pt-[1.1875rem] md:pt-[2.125rem] pb-[1.0625rem] md:pb-[1.875rem] w-10 md:w-[5.4375rem] xl:w-[7rem]",
+        "flex justify-center bg-darkNavy relative rounded-xl md:rounded-3xl xl:rounded-[2.5rem] pt-[1.1875rem] md:pt-[2.125rem] pb-[1.0625rem] md:pb-[1.875rem] w-10 md:w-[5.4375rem] xl:w-[7rem]",
         {
           "brightness-50": disabled,
         }
       )}
     >
-      <div className="flex justify-center absolute rounded-[0.75rem] md:rounded-[2.5rem] bg-blue-500 top-[0.0625rem] bottom-[0.3125rem] left-[0.1875rem] right-[0.1875rem]">
-        <div className="flex justify-center absolute top-[0.375rem] bottom-0 rounded-[0.75rem] md:rounded-[2.5rem] bg-playBlue left-[0.1875rem] right-[0.1875rem]" />
+      <div className="flex justify-center absolute rounded-xl md:rounded-3xl xl:rounded-[2.5rem] bg-blue-500 top-[0.0625rem] bottom-[0.3125rem] left-[0.1875rem] right-[0.1875rem]">
+        <div className="flex justify-center absolute top-[0.375rem] bottom-0 rounded-xl md:rounded-3xl xl:rounded-[2.5rem] bg-playBlue left-[0.1875rem] right-[0.1875rem]" />
       </div>
       <h2
         className={cn(
