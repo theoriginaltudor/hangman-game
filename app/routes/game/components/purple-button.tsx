13a8df594d204af487b4daa2e920370bbf3e3a73
theoriginaltudor <theoriginaltudor@gmail.com>
2025-01-31 14:38:19 +0100

    feat: remove prefetch attribute from PurpleButton component for improved performance

--- app/routes/game/components/purple-button.tsx
+++ app/routes/game/components/purple-button.tsx
@@ -10,7 +10,6 @@ export const PurpleButton: React.FC<Props> = ({ children, to: link, ref }) => {
     <Link
       to={link}
       ref={ref}
-      prefetch="render"
       className="relative flex justify-center px-16 py-5 rounded-full bg-darkNavy hover:brightness-105"
     >
       <div className="flex justify-center absolute rounded-full bg-purple-600 top-[0.0625rem] bottom-[0.3125rem] left-[0.1875rem] right-[0.1875rem]">
